ecb67cde50b1795f9f7062a6ba9526265bad2d2b
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2015-08-28 13:01:17 +0000

    *** empty log message ***

--- src/de/webis/query/segmentation/core/QueryHelper.java
+++ src/de/webis/query/segmentation/core/QueryHelper.java
@@ -1,13 +1,10 @@
 package de.webis.query.segmentation.core;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class QueryHelper {
