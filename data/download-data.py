a7568d3f729647030dd7bf81346b874a93400ffa
Maik Fröbe <maik.froebe@uni-jena.de>
2023-11-05 16:42:50 +0100

    mf

--- data/download-data.py
+++ data/download-data.py
@@ -4,9 +4,9 @@ import shutil
 tira = Client()
 
 
-truth_ground_dir = tira.download_dataset('ir-lab-jena-leipzig-wise-2023', 'training-20231104-training', truth_dataset=True)
-shutil.copyfile(truth_ground_dir + '/queries.jsonl', 'ir-lab-sose-training/input/queries.jsonl')
+ground_truth_dir = tira.download_dataset('ir-lab-jena-leipzig-wise-2023', 'training-20231104-training', truth_dataset=True)
+shutil.copyfile(ground_truth_dir + '/queries.jsonl', 'ir-lab-sose-training/input/queries.jsonl')
 
 ground_truth_dir = tira.download_dataset('ir-lab-jena-leipzig-wise-2023', 'validation-20231104-training', truth_dataset=True)
-shutil.copyfile(truth_ground_dir + '/queries.jsonl', 'ir-lab-sose-validation/input/queries.jsonl')
+shutil.copyfile(ground_truth_dir + '/queries.jsonl', 'ir-lab-sose-validation/input/queries.jsonl')
 
